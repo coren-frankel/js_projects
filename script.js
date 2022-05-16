d31a32906e437e4d9c82aab0ed30ff292d4d7a8c
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-05-16 16:59:37 -0600

    Draft psuedocode for roshambo game

---
+++ script.js
@@ -0,0 +1,23 @@
+let playerSelection = x
+//Allow for user to type Rock, Paper, or Scissors, assigning the value to playerSelection
+//Once the playerSelection box is filled the player clicks 'Submit' to continue.
+//Upon clicking 'Submit' the entry is changed to lower case and the function computerPlay 
+  //randomly chooses 'Rock' 'Paper' or 'Scissors' and returns the result.
+let computerSelection = ('Rock' || 'Paper' || 'Scissors')
+//
+function computerPlay(random.computerSelection) {
+
+}
+
+addEventListener()
+function roShamBo(x, y) {
+
+}
+//Take the random selection from computerPlay and match it against playerSelection
+//If x == y, then print 'Draw! Try Again!'
+//Else if x = Rock && y = Paper, then print 'You Lose! Paper beats Rock!'
+//Else if x = Rock && y = Scissors, then print 'You Win! Rock beats Scissors!'
+//Else if x = Paper && y = Rock, then print 'You Win! Paper beats Rock!'
+//Else if x = Paper && y = Scissors, then print 'You Lose! Scissors beats Paper!'
+//Else if x = Scissors && y = Rock, then print 'You Lose! Rock beats Scissors!'
+//Else if x = Scissors && y = Paper, then print 'You Win! Scissors beats Paper!'
