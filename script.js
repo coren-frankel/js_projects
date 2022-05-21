e00f409cfab901e0f26f96eeb083aad4d65313ce
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-05-21 16:50:48 -0600

    Rework base functions, game function still in works

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
-let playerSelection;
-const textArray = ['rock', 'paper', 'scissors'];
-const randomIndex = Math.floor(Math.random() * textArray.length);
-const computerSelection = textArray[randomIndex];
+//Allow function computerPlay to randomly choose 'Rock' 'Paper' or 'Scissors' and return the result
+const choice = ['rock', 'paper', 'scissors'];
 function computerPlay() {
-  return computerSelection;
+    let computerRandom = choice[Math.floor(Math.random() * choice.length)];
+    return computerRandom;
 }
-console.log(computerPlay());
-function submit() {
-  playerSelection = document.getElementById("box").value.toLowerCase();
-  console.log(playerSelection);
-  playGame();
-  function playGame(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        alert(draw);
-    } else if ((playerSelection = 'rock') && (computerSelection = 'scissors')) {
-        alert(winRBS);
-    } else if ((playerSelection = 'paper') && (computerSelection = 'rock')) {
-        alert(winPBR);
-    } else if ((playerSelection = 'paper') && (computerSelection = 'scissors')) {
-        alert(loseSBP);
-    } else if ((playerSelection = 'scissors') && (computerSelection = 'rock')) {
-        alert(loseRBS);
-    } else if ((playerSelection = 'scissors') && (computerSelection = 'paper')) {
-        alert(winSBP);
-    } else if ((playerSelection = 'rock') && (computerSelection = 'paper')) {
-        alert(losePBR);
-    } else (playerSelection != ('rock' || 'paper' || 'scissors')) 
-        alert(error);
-  }
-}
-const draw = ('Draw! Try Again!');
-const error = ('Error: Incompatible Entry');
-const losePBR = ('You Lose! Paper beats Rock!');
-const winRBS = ('You Win! Rock beats Scissors!');
-const winPBR = ('You Win! Paper beats Rock!');
-const loseSBP = ('You Lose! Scissors beats Paper!');
-const loseRBS = ('You Lose! Rock beats Scissors!');
-const winSBP = ('You Win! Scissors beats Paper!');
+const computerSelection = computerPlay();
+console.log(computerSelection)
+
 //Take the random selection from computerPlay and match it against playerSelection
 //If x == y, then print 'Draw! Try Again!'
 //Else if x = Rock && y = Paper, then print 'You Lose! Paper beats Rock!'
@@ -44,12 +14,81 @@ const winSBP = ('You Win! Scissors beats Paper!');
 //Else if x = Paper && y = Rock, then print 'You Win! Paper beats Rock!'
 //Else if x = Paper && y = Scissors, then print 'You Lose! Scissors beats Paper!'
 //Else if x = Scissors && y = Rock, then print 'You Lose! Rock beats Scissors!'
-//Else if x = Scissors && y = Paper, then print 'You Win! Scissors beats Paper!'/
+//Else if x = Scissors && y = Paper, then print 'You Win! Scissors beats Paper!'
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {
+        playerScore += 1;
+        return 'You Win! Rock beats Scissors!';
+    } else if ((playerSelection == 'paper') && (computerSelection == 'rock')) {
+        playerScore += 1;
+        return 'You Win! Paper beats Rock!'; 
+    } else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {
+        computerScore += 1; 
+        return 'You Lose! Scissors beats Paper!';
+    } else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {
+        computerScore += 1;
+        return 'You Lose! Rock beats Scissors!';
+    } else if ((playerSelection == 'scissors') && (computerSelection == 'paper')) {
+        playerScore += 1;
+        return 'You Win! Scissors beats Paper!';
+    } else if ((playerSelection == 'rock') && (computerSelection == 'paper')) {
+        computerScore += 1;
+        return 'You Lose! Paper beats Rock!';
+    } else if (playerSelection == computerSelection) {
+        return 'Draw! Try Again!';
+    } else {
+        return error;
+    }
+}
 
 
 //Allow for user to type Rock, Paper, or Scissors, assigning the value to playerSelection
 //Once the playerSelection box is filled the player clicks 'Submit' to continue.
 //Upon clicking 'Submit' the entry is changed to lower case 
-//Allow function computerPlay to randomly choose 'Rock' 'Paper' or 'Scissors' and return the result.
+const error = "Your entry does not work, please refresh page and try again!";
+function soloPlay() {
+    const playerEntry = prompt("Choose Rock, Paper, or Scissors:", "1,2,3, shoot...");
+    const playerCase = playerEntry.toLowerCase();
+    switch(playerCase) {
+        case "rock":
+            return playerCase;
+            break;
+        case "paper":
+            return playerCase;
+            break;
+        case "scissors":
+            return playerCase;
+            break;
+        default:
+            alert(error);
+    }
+}
+const playerSelection = soloPlay();
+console.log(playerSelection)
+console.log(playRound(playerSelection, computerSelection));
 
+//Create game function that uses playRound function 5 times
+//Use loop to keep track of 5 rounds scores
+//If player wins, 1 point gained
+//If player loses, computer gains 1 point
+//If Draw, no points gained
+//After 5 rounds compare values  
+//Deliver You Win or You Lose game message
 
+let playerScore = 0;
+let computerScore = 0;
+
+function game(playRound) {
+    let x = playerScore;
+    let y = computerScore;
+    for (let i = 0; i < 5; i++) {
+        if ((x + y) > 4) {
+            return score;
+        } else {
+            playRound();
+        }
+    }
+    let result = (x > y) ? 'Win': 'Lose';
+    score = `Player 1: {playerScore} Computer: {computerScore}, You {result}!`
+}
+game();
