065a458a0c44b48ec00abf706eb0732a0f12fcd0
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-14 23:37:38 -0600

    Zero in on repairing game() Focus on score and result DOM methods next.

--- script.js
+++ script.js
@@ -26,37 +26,46 @@ function game() {
         //Take the random selection from computerPlay and match it against playerSelection
             if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {
                 ++playerScore;//If player wins, 1 point gained
-                result = `You Win! Rock beats Scissors!`;
+                return result = `You Win! Rock beats Scissors!`;
                 //Else if x = Rock && y = Scissors, then print 'You Win! Rock beats Scissors!'
             } else if ((playerSelection == 'paper') && (computerSelection == 'rock')) {
                 ++playerScore;//Else if x = Paper && y = Rock, then print 'You Win! Paper beats Rock!'
-                result = `You Win! Paper beats Rock!`; 
+                return result = `You Win! Paper beats Rock!`; 
             } else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {
                 ++computerScore;//If player loses, computer gains 1 point
-                result = `You Lose! Scissors beats Paper!`;
+                return result = `You Lose! Scissors beats Paper!`;
                 //Else if x = Paper && y = Scissors, then print 'You Lose! Scissors beats Paper!'
             } else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {
                 ++computerScore;//Else if x = Scissors && y = Rock, then print 'You Lose! Rock beats Scissors!'
-                result = `You Lose! Rock beats Scissors!`;
+                return result = `You Lose! Rock beats Scissors!`;
             } else if ((playerSelection == 'scissors') && (computerSelection == 'paper')) {
                 ++playerScore;//Else if x = Scissors && y = Paper, then print 'You Win! Scissors beats Paper!'
-                result =  `You Win! Scissors beats Paper!`;
+                return result =  `You Win! Scissors beats Paper!`;
             } else if ((playerSelection == 'rock') && (computerSelection == 'paper')) {
                 ++computerScore;//Else if x = Rock && y = Paper, then print 'You Lose! Paper beats Rock!'
-                result = `You Lose! Paper beats Rock!`;
+                return result = `You Lose! Paper beats Rock!`;
             } else if (playerSelection == computerSelection) {
                 i--;//If Draw or Error, no points gained
-                result = `Draw! Try Again!`;//If x == y, then print 'Draw! Try Again!'
+                return result = `Draw! Try Again!`;//If x == y, then print 'Draw! Try Again!'
             }
         }
-        function play(e) {
-            playerSelection = `${img.id}`;
-            console.log(playerSelection);
-            console.log(playRound(playerSelection, computerSelection));
+        //function () {
+            
+            //console.log(playRound(playerSelection, computerSelection));
             //playRound();
-        }
+            
+        //}
         imgs.forEach((img) => {
-            img.addEventListener('click', play)}); // we use the .forEach method to iterate through each img
+            img.addEventListener('click', (e) => {playerSelection = e.target.id;
+                console.log(playerSelection);
+                computerPlay();
+                console.log(computerSelection);
+                playRound();
+                console.log(score);
+                console.log(result);
+            
+            })}); 
+        // we use the .forEach method to iterate through each img
         // and for each one we add a 'click' listener  
         //Allow for user to click an image to choose Rock, Paper, or Scissors 
         //Once the player clicks an image, the playerSelection is made and assigned with playRound.
@@ -66,11 +75,11 @@ function game() {
         //Deliver You Win or You Lose game message
         switch (i <= 4) {
             case (i < 5):
-                score = `Home:${playerScore}${gap}Away:${computerScore}`;
-                result = `You played ${playerSelection} against the Computer's ${computerSelection}${gap}${playRound(playerSelection, computerSelection)}`;
+                score.textContent = `Home:${playerScore}${gap}Away:${computerScore}`;
+                result.textContent = `You played ${playerSelection} against the Computer's ${computerSelection}${gap}${playRound(playerSelection, computerSelection)}`;
             default:
-                score = `Home:${playerScore}${gap}Away:${computerScore}`;
-                result = `Game Over${gap}Congratulations, you ${grats}!`
+                score.textContent = `Home:${playerScore}${gap}Away:${computerScore}`;
+                result.textContent = `Game Over${gap}Congratulations, you ${grats}!`
         }
     }
 };
